836b395e7aa53dd214fb71996f938862c0009293
rungphob2545 <rungphob.2545@mail.kmutt.ac.th>
2022-04-29 15:42:10 +0700

    test1
    just a test commit

--- OASIP_Project/src/App.vue
+++ OASIP_Project/src/App.vue
@@ -1,21 +1,55 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Table from './components/Table.vue';
+
 </script>
-
+ 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class ="container1">Event Databased</div>
+  <div>
+    <h1>Add Event</h1>
+    <svg xmlns="http://www.w3.org/2000/svg" 
+    width="70" 
+    height="70" 
+    fill="currentColor" 
+    class="bi bi-calendar-event" 
+    viewBox="0 0 16 16"
+    color="#FD8D8D">
+  <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+</svg>
+    <h1>Add Catagory</h1>
+    <svg xmlns="http://www.w3.org/2000/svg" 
+    width="70" 
+    height="70" 
+    fill="currentColor" 
+    class="bi bi-folder-plus" 
+    viewBox="0 0 16 16"
+    color="#FD8D8D">
+  <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
+  <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
+</svg>
+   </div>
+  <div  class="container2">
+    <Table />
+  </div>
 </template>
-
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+.container1 {
+ background-color: #FD8D8D;
+ font-size: 30px;
+ height: 50px;
+ padding: 5px 0px 0px 30px;
+}
+
+.container2 {
+  height: 500px;
+  width:1400px;
+  background-color: #FD8D8D;
+  border-radius: 20%;
+  margin-left: 70px;
+  
 }
 </style>
---
+++ OASIP_Project/src/components/Table.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue';
+
+const datas =  ref([
+  {name: 'aaa', catagory:'zzz', stTime:12.30, duration:'30m',ooo:'zzz'},
+  {name: 'avc', catagory:'zz1', stTime:12.30, duration:'30m',ooo:'zzz'},
+  {name: 'abc', catagory:'zz3', stTime:12.30, duration:'30m',ooo:'zzz'}
+])
+</script>
+ 
+<template>
+  <div>
+    <div v-if="datas.length=0">
+    <h1 class="ne">No Event Schudule Today</h1>
+    </div>
+    <table v-else>
+      <thead>
+        <tr>
+          <th>Booking Name</th>
+          <th>Catagory</th>
+          <th>Start Up Time</th>
+          <th>Duration</th>
+          <th>ooo</th>
+        </tr>
+      </thead>
+      <tbody v-for="data in datas" :key="data.name">
+        <th>{{data.name}}</th>
+        <th>{{data.catagory}}</th>
+        <th>{{data.stTime}}</th>
+        <th>{{data.duration}}</th>
+        <th>{{data.ooo}}</th>
+      </tbody>
+    </table>
+  </div>
+</template>
+ 
+<style>
+.ne {
+  font-size: 600%;
+  padding: 10% 0px 0px 10%;
+  color:white;
+  font-style: italic;
+}
+th {
+  font-size:30px;
+  padding-left: 100px;
+  padding-top: 30px;
+}
+</style>
