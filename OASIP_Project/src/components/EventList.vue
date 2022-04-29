662fc57ccb21936e64f9c4f221245063ad21a1e5
Nabin-a <nabin.ameen@gmail.com>
2022-04-29 21:52:00 +0700

    modify backend frontend

---
+++ OASIP_Project/src/components/EventList.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  eventList: {
+    type: Array,
+    require: true
+  }
+})
+</script>
+ 
+<template>
+<h2>Event List</h2>
+<p v-if="eventList.length < 1">No schedule events</p>
+    <ul v-else>
+      <li v-for="(event, index) in eventList" :key="index">
+        Id: {{ event.id }}, Name: {{ event.bookName }}, 
+        Email: {{ event.bookingEmail }}, Start Time: {{ event.startTime }},
+        Note: {{event.note}} ,Catagoty: {{ event.catagoty }} 
+      </li>
+    </ul>
+</template>
+ 
+<style>
+
+</style>
---
+++ OASIP_Project/src/views/Mytable.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, onBeforeMount } from 'vue';
+import EventList from '../components/EventList.vue';
+const events = ref([])
+
+onBeforeMount(async () => {
+  await getNotes()
+  console.log(events.value)
+})
+
+const getNotes = async () => {
+  const res = await fetch('http://localhost:8080/api/event')
+  if (res.status === 200) {
+    events.value = await res.json()
+    console.log(events.value)
+    return events.value
+  } else console.log('error, cannot get notes')
+}
+
+
+</script>
+
+<template>
+    <EventList
+        :eventList="events"
+    />
+</template>
+ 
+<style>
+
+</style>
