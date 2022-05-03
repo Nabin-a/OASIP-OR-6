0851d37311716cd8ba707f3398f375c65d446bba
Nabin-a <nabin.ameen@gmail.com>
2022-05-03 22:26:13 +0700

    import CORS in back

--- OASIP-backend/src/main/java/sit/int221/oasip/controllers/EventController.java
+++ OASIP-backend/src/main/java/sit/int221/oasip/controllers/EventController.java
@@ -2,6 +2,7 @@ package sit.int221.oasip.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import sit.int221.oasip.repositories.EventRepository;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/event")
 public class EventController {
