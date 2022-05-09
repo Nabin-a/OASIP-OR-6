b034ef11eb27b5c569baa8bb216076e23abd660d
Nabin-a <nabin.ameen@gmail.com>
2022-05-09 20:25:23 +0700

    import Annotation

--- OASIP-backend/src/main/java/sit/int221/oasip/controllers/EventController.java
+++ OASIP-backend/src/main/java/sit/int221/oasip/controllers/EventController.java
@@ -2,11 +2,8 @@ package sit.int221.oasip.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import sit.int221.oasip.DTO.EventDtoCreate;
 import sit.int221.oasip.DTO.EventDtoDetail;
