024bf8cbd41e929ca8c9f65a940531fe13df2225
Nabin-a <nabin.ameen@gmail.com>
2022-08-29 11:25:07 +0700

    add column password

--- OASIP-db/user.sql
+++ OASIP-db/user.sql
@@ -1,8 +1,8 @@
-insert into user (name, email, role, createdOn, updatedOn)
-values('OASIP ADMIN', 'oasip.admin@kmutt.ac.th', 'admin', '2022-08-01 00:00:00+07:00', '2022-08-01 00:00:00+07:00'),
-	('Somchai Jaidee', 'somchai.jai@kmutt.ac.th', 'lecturer', '2022-08-08 15:00:00+07:00', '2022-08-08 15:00:00+07:00'),
-	('Komkrid Rakdee', 'komkrid.rak@mail.kmutt.ac.th', 'student', '2022-08-08 15:00:01+07:00', '2022-08-08 15:00:01+07:00'),
-    ('สมเกียรติ ขยันเรียน', 'somkiat.kay@kmutt.ac.th', 'student', '2022-08-16 09:00:00+07:00', '2022-08-16 09:00:00+07:00');
+insert into user (name, email, password, role, createdOn, updatedOn)
+values('OASIP ADMIN', 'oasip.admin@kmutt.ac.th', 'DdVH/5s/qS6onDKFopsnuLoQgKJsJbB0BVxCiAxthUA=', 'admin', '2022-08-01 00:00:00+07:00', '2022-08-01 00:00:00+07:00'),
+	('Somchai Jaidee', 'somchai.jai@kmutt.ac.th', 'Lu0GBjygeRYP/GE3Mxfrp2j0CR3x0YLjTgfjRvsdpoU=', 'lecturer', '2022-08-08 15:00:00+07:00', '2022-08-08 15:00:00+07:00'),
+	('Komkrid Rakdee', 'komkrid.rak@mail.kmutt.ac.th', '6XC664Enif8fvOiKMAdT+hw/SIWAqhYb2SQpFa8tZOE=', 'student', '2022-08-08 15:00:01+07:00', '2022-08-08 15:00:01+07:00'),
+    ('สมเกียรติ ขยันเรียน', 'somkiat.kay@kmutt.ac.th', 'QW8O/BetAnyoKU+lf7ludTR4G1q4fxGBUbooUMPzrA4=', 'student', '2022-08-16 09:00:00+07:00', '2022-08-16 09:00:00+07:00');
 
 
 
